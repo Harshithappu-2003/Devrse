254a99b56333ad6ffca951b8e04485a295b8f09b
Harshithappu-2003 <harshithparam20@gmail.com>
2025-05-04 23:44:49 +0530

    playing with the routes

--- src/app.js
+++ src/app.js
@@ -2,28 +2,10 @@ const express = require('express'); // Import express
 
 const app = express(); // Create an instance of express
 
-app.use("/user", (req, res) => {
-    res.send("HAHAHAHAHAHA");
-});
 
-// This will only handle GET call to /user
-app.get("/user", (req, res) => {
-    res.send({ firstName: "Harshith", lastName: "P" });
-}); 
-
-app.post("/user", (req, res) => {
-    // saving data to db
-    res.send("Data successfully saved to the database");
-});
-
-app.delete("/user", (req, res) => {
-    // deleting data from db
-    res.send("Data successfully deleted from the database");
-});
-
-// this will match all the http method API calls to /test
-app.use("/test", (req, res) => {
-    res.send('Hello World!');
+app.get("/user/:userId/:name/:password", (req, res) => {
+  console.log(req.params);
+  res.send({ firstName: "Harshith", lastName: "P" });
 });
 
 
