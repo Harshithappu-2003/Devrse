4c23279406ec6df6b2db6414eeac23e8f4268755
Harshithappu-2003 <harshithparam20@gmail.com>
2025-05-04 18:22:27 +0530

    exploring get,post ,delete

--- src/app.js
+++ src/app.js
@@ -2,13 +2,30 @@ const express = require('express'); // Import express
 
 const app = express(); // Create an instance of express
 
+app.use("/user", (req, res) => {
+    res.send("HAHAHAHAHAHA");
+});
+
+// This will only handle GET call to /user
+app.get("/user", (req, res) => {
+    res.send({ firstName: "Harshith", lastName: "P" });
+}); 
+
+app.post("/user", (req, res) => {
+    // saving data to db
+    res.send("Data successfully saved to the database");
+});
+
+app.delete("/user", (req, res) => {
+    // deleting data from db
+    res.send("Data successfully deleted from the database");
+});
+
+// this will match all the http method API calls to /test
 app.use("/test", (req, res) => {
     res.send('Hello World!');
 });
 
-app.use("/Hello", (req, res) => {
-    res.send('Hello From the server!');
-});
 
 app.listen(3000, () => { // Start the server on port 3000
     console.log('Server is running on port 3000....');
